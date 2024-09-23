7f9e80ef3ece0bddc89c3264baef28bc7d8a3bbd
Gleb Pokotilo <pokotilo.gleb1@gmail.com>
2024-09-23 22:07:51 +0300

    feat: sort popup and top container

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { Categories, Container, Title } from "@/components/shared";
+import { Container, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
-    <Container className="mt-8">
-      <Title text="Все пиццы" size="lg" className="font-extrabold" />
-      <Categories />
-    </Container>
+    <>
+      <Container className="mt-8">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+      <div style={{ height: '10000px' }} />
+    </>
   );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,3 +2,5 @@ export { Header } from './header';
 export { Categories } from './categories';
 export { Container } from './container';
 export { Title } from './title';
+export { TopBar } from './topbar';
+export { SortPopup } from './sort-popup';
